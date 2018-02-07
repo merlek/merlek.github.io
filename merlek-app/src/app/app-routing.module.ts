b32967dcad3f6fd1f40b974945ba15155d213cb2
Merle Klein <merlek@wolfram.com>
2018-02-06 20:46:25 -0500

    switched to HashLocationStrategy because of GitHub.io

--- merlek-app/src/app/app-routing.module.ts
+++ merlek-app/src/app/app-routing.module.ts
@@ -1,17 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 import { TransliterateComponent } from './transliterate/transliterate.component';
 import { GreekVocabComponent } from './greek-vocab/greek-vocab.component';
 
 const routes: Routes = [
-	{ path: '', redirectTo: '/transliterate', pathMatch: 'full' },
+  { path: '', redirectTo: '/transliterate', pathMatch: 'full' },
   { path: 'transliterate', component: TransliterateComponent },
   { path: 'greek-vocab', component: GreekVocabComponent },
 ];
 
 @NgModule({
-	imports: [ RouterModule.forRoot(routes) ],
-  exports: [ RouterModule ]
+  imports: [ RouterModule.forRoot(routes, {useHash: true}) ],
+  exports: [ RouterModule ],
+  providers: [{provide: LocationStrategy, useClass: HashLocationStrategy}]
 })
 export class AppRoutingModule {}
